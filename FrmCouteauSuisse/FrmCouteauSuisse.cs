a127fc1ba585cd521227d5b2c852978e7d998fb2
Sevenn <thomas.s833@gmail.com>
2016-04-27 11:41:00 +0200

    Form non sizable

---
+++ FrmCouteauSuisse/FrmCouteauSuisse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmCouteauSuisse
+{
+    public partial class FrmCouteauSuisse : Form
+    {
+        public FrmCouteauSuisse()
+        {
+            InitializeComponent();
+        }
+    }
+}
